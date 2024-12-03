cec1ca1f0a82e2b6d258c582341ac7fb91f0a0e6
Nicholas Weiss <njweiss4@gmail.com>
2024-12-03 10:10:31 -0500

    day 3

--- day3.py
+++ day3.py
@@ -3,13 +3,12 @@ import re
 
 
 def part1(memory):
-    matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)", memory)
-    return sum(int(a) * int(b) for a, b in (match.split('(')[1][:-1].split(',') for match in matches))
+    return sum(int(a) * int(b) for a, b in (match.split('(')[1][:-1].split(',')
+                                            for match in re.findall(r"mul\(\d{1,3},\d{1,3}\)", memory)))
 
 
 def part2(memory):
-    do_sections = re.split(r"don't\(\)[\s\S]*?(do\(\)|$)", memory)
-    return sum(part1(do_section) for do_section in do_sections)
+    return sum(part1(do_section) for do_section in re.split(r"don't\(\)[\s\S]*?(do\(\)|$)", memory))
 
 
 input_str = open("inputs/day3.txt", 'r').read()
